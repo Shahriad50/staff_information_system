79d22587fd0095191145f758419119af3e643400
jahedulsumon32 <jahedulsumon32@gmail.com>
2024-04-08 02:09:49 +0600

    Adding staff details page

--- src/Registrar.js
+++ src/Registrar.js
@@ -1,48 +1,103 @@
 import React from "react";
-import "./css/card.css"; // Import CSS file for styling
+import { Link } from "react-router-dom";
 import Staff from "./Staff";
 import staffImage1 from "./assets/pic1.jpg"; // Import the image file
 import staffImage2 from "./assets/pic2.jpg"; // Import the image file
 import staffImage3 from "./assets/pic3.jpg"; // Import the image file
+import "./css/card.css"; // Import CSS file for styling
 
 const Registrar = () => {
   // Sample staff data
   const staff1 = {
-    image: staffImage1, // Use the imported image directly
-    name: "John Doe",
+    image: staffImage1,
+    name: {
+      english: "John Doe",
+      bengali: "জন ডো",
+    },
     designation: "Senior Developer",
+    education: "Bachelor of Science in Computer Science",
+    joinDate: "2022-01-01",
+    phone: "+1234567890",
+    email: "john.doe@example.com",
+    gender: "Male",
+    bloodGroup: "A+",
+    religion: "Christianity",
+    dob: "1990-05-15",
+    nationality: "American",
+    ethnicity: "Caucasian",
   };
   const staff2 = {
-    image: staffImage2, // Use the imported image directly
-    name: "Alandro G ",
+    image: staffImage2,
+    name: {
+      english: "Alandro G",
+      bengali: "আলানদ্রো জি",
+    },
     designation: "Developer",
+    education: "Bachelor of Engineering in Software Engineering",
+    joinDate: "2022-03-15",
+    phone: "+1987654321",
+    email: "alandro.g@example.com",
+    gender: "Male",
+    bloodGroup: "B-",
+    religion: "Islam",
+    dob: "1995-08-20",
+    nationality: "Canadian",
+    ethnicity: "Hispanic",
   };
 
   const staff3 = {
-    image: staffImage3, // Use the imported image directly
-    name: "Tom Hardy",
+    image: staffImage3,
+    name: {
+      english: "Tom Hardy",
+      bengali: "টম হার্ডি",
+    },
     designation: "Junior Developer",
+    education: "Bachelor of Technology in Information Technology",
+    joinDate: "2023-02-10",
+    phone: "+1122334455",
+    email: "tom.hardy@example.com",
+    gender: "Male",
+    bloodGroup: "O+",
+    religion: "Judaism",
+    dob: "1998-11-25",
+    nationality: "British",
+    ethnicity: "White",
+  };
+  const linkStyle = {
+    textDecoration: "none", // Remove underline
+    color: "black", // Set text color to black // Prevent reduction in card width caused by the link
+    width: "50%", // Set width to 100% to avoid affecting the card width
   };
 
   return (
     <div className="card-container">
-      {/* Pass staff data as props to the Staff component */}
-      <Staff
-        image={staff1.image}
-        name={staff1.name}
-        designation={staff1.designation}
-      />
-      <Staff
-        image={staff2.image}
-        name={staff2.name}
-        designation={staff2.designation}
-      />
-
-      <Staff
-        image={staff3.image}
-        name={staff3.name}
-        designation={staff3.designation}
-      />
+      <Link
+        to={{
+          pathname: "/dashboard/office/register/staff1",
+          state: { staff: staff1 },
+        }}
+        style={linkStyle}
+      >
+        <Staff staff={staff1} />
+      </Link>
+      <Link
+        to={{
+          pathname: "/dashboard/office/register/staff2",
+          state: { staff: staff2 },
+        }}
+        style={linkStyle}
+      >
+        <Staff staff={staff2} />
+      </Link>
+      <Link
+        to={{
+          pathname: "/dashboard/office/register/staff3",
+          state: { staff: staff3 },
+        }}
+        style={linkStyle}
+      >
+        <Staff staff={staff3} />
+      </Link>
     </div>
   );
 };
