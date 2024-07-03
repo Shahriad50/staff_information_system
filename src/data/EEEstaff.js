3919f0e3164464c102df2d36550d2e698dbcc948
jahedulsumon32 <jahedulsumon32@gmail.com>
2024-07-03 18:45:44 +0600

    Modifying info

---
+++ src/data/EEEstaff.js
@@ -0,0 +1,59 @@
+import noimage from "../assets/noimage.jpg"; // Import the image file
+const staffData = [
+  {
+    image: noimage,
+    name: {
+      english: "Mr. Samsu Uddin Ahmed",
+      bengali: "জনাব সামসু উদ্দিন আহমেদ",
+    },
+    designation: "ডেপুটি রেজিস্ট্রার",
+    Office: "/4425",
+    education: "N/A",
+    joinDate: "N/A",
+    phone: "01819008494",
+    email: "N/A",
+    gender: "Male",
+    bloodGroup: "N/A",
+    religion: "Islam",
+    dob: "N/A",
+    nationality: "Bangladeshi",
+  },
+  {
+    image: noimage,
+    name: {
+      english: "Mr. Abdul Mannan",
+      bengali: "জনাব আব্দুল মান্নান",
+    },
+    designation: "সহকারী রেজিস্ট্রার (সেমিনার)",
+    Office: "/4425",
+    education: "N/A",
+    joinDate: "N/A",
+    phone: "01837463626",
+    email: "mannancu14@gmail.com",
+    gender: "Male",
+    bloodGroup: "N/A",
+    religion: "Islam",
+    dob: "N/A",
+    nationality: "Bangladeshi",
+  },
+  {
+    image: noimage,
+    name: {
+      english: "Mr. Mohammad Mafizul Hoque",
+      bengali: "জনাব মোহাম্মদ মফিজুল হক",
+    },
+    designation: "সেকশন অফিসার",
+    Office: "/4425",
+    education: "N/A",
+    joinDate: "N/A",
+    phone: "01858362626",
+    email: "mafiz@cu.ac.bd or hoquemafiz@gmail.com",
+    gender: "Male",
+    bloodGroup: "N/A",
+    religion: "Islam",
+    dob: "N/A",
+    nationality: "Bangladeshi",
+  },
+];
+
+export default staffData;
