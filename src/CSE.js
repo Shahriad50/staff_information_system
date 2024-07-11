c835df027937d10bc76a302619a61f735c5b50b6
jahedulsumon32 <jahedulsumon32@gmail.com>
2024-07-11 20:14:17 +0600

    Fetching staff from db

--- src/CSE.js
+++ src/CSE.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import Staff from "./Staff";
-import CSEStaffData from "./data/CSEstaff"; // Import the data fil
+import CSEStaffData from "./data/CSEstaff"; // Import the data file
 import "./css/card.css"; // Import CSS file for styling
 import "./css/staff.css";
 
 const CSE = () => {
+  const [additionalStaff, setAdditionalStaff] = useState([]);
+
   const linkStyle = {
     textDecoration: "none", // Remove underline
     color: "black", // Set text color to black // Prevent reduction in card width caused by the link
@@ -15,10 +18,36 @@ const CSE = () => {
     return name.replace(/\s+/g, "-"); // Replace spaces with dashes
   };
 
+  useEffect(() => {
+    const fetchAdditionalStaff = async () => {
+      try {
+        const response = await axios.get(
+          "http://api.bike-csecu.com/api/staff/department/CSE"
+        );
+        const staffData = response.data.data.map((staff) => ({
+          name: {
+            bengali: `${staff.first_name_bn} ${staff.last_name_bn}`,
+            english: `${staff.first_name} ${staff.last_name}`,
+          },
+          image: staff.profile_image_id, // Assuming this ID can be used to get the image URL
+          designation: staff.role,
+          ...staff, // Add other fields if needed
+        }));
+        setAdditionalStaff(staffData);
+      } catch (error) {
+        console.error("Error fetching additional staff:", error);
+      }
+    };
+
+    fetchAdditionalStaff();
+  }, []);
+
+  const allStaffData = [...CSEStaffData, ...additionalStaff];
+
   return (
     <div className="card-container">
       <div className="staffcard-container">
-        {CSEStaffData.map((staff, index) => (
+        {allStaffData.map((staff, index) => (
           <Link
             key={index}
             to={{
