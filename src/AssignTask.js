c2a06b30a2e9e4754e2608359624a31a12e0529d
Shahriad50 <82698995+Shahriad50@users.noreply.github.com>
2024-07-13 23:42:36 +0600

    Revert ""some design added""

--- src/AssignTask.js
+++ src/AssignTask.js
@@ -9,10 +9,11 @@ const StaffDropdown = () => {
   const [selectedStaff, setSelectedStaff] = useState("");
   const [taskDescription, setTaskDescription] = useState("");
   const [dueDate, setDueDate] = useState("");
-  const [taskTitle, setTaskTitle] = useState("");
-  const [message, setMessage] = useState(""); 
+  const [title, setTitle] = useState("");
+  const [taskAttachment, setTaskAttachment] = useState(null);
+  const [message, setMessage] = useState("");
   const [staffList, setStaffList] = useState([]);
-  const [department,setDepartment]=useState("EEE");
+  const [department, setDepartment] = useState("CSE");
 
   // Utility function to convert file to base64
   const toBase64 = (file) => new Promise((resolve, reject) => {
@@ -54,50 +55,9 @@ const StaffDropdown = () => {
   };
 
   const handleTitleChange = (e) => {
-    setTaskTitle(e.target.value);
+    setTitle(e.target.value);
   };
 
-
- 
-  
-    const handleAssign = async () => {
-      if (!selectedStaff || !taskDescription || !dueDate ) {
-        setMessage("All fields are required.");
-        return;
-      }
-      const newTask = {
-        assign_to: selectedStaff,
-        assign_date: new Date().toISOString(),
-        due_date: dueDate,
-        task_title: taskTitle,
-        task_description: taskDescription,
-        task_attachment: "", // Add attachment logic if needed
-        task_status: 0 // Default status for new task
-      };
-      const config ={
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${token}` 
-        },
-      }
-      try{
-        const response= await axios.post("http://api.bike-csecu.com/api/task/add",newTask,config);
-        console.log(response.data);
-        if(response.data){
-            setMessage(`Task added to  successfully.`);
-            setSelectedStaff("");
-            setTaskDescription("");
-            setDueDate("");
-            setTaskTitle("");
-          } else {
-            setMessage("Failed to add task. Please try again.");
-          }
-        }
-      catch(error){
-        console.error("Error assigning task:", error);
-        setMessage("Failed to assign task. Please try again.");
-        return;
-=======
   const handleFileChange = async (e) => {
     const file = e.target.files[0];
     if (file && file.type === "application/pdf") {
@@ -156,64 +116,6 @@ const StaffDropdown = () => {
   };
 
   return (
-
-    <div className=" row card-container">
-      <div className="staff-dropdown-container">
-        <div className="assign-task-content">
-          <h2>Assign Task</h2>
-          {message && <p>{message}</p>}
-          <div className="form-group">
-            <label className="label" htmlFor="staff">
-              Select Staff:
-            </label>
-            <select
-              id="staff"
-              value={selectedStaff}
-              onChange={handleStaffChange}
-              className="select-box"
-            >
-              <option value="">Choose staff member</option>
-              {staffList.map((staff) => (
-          <option key={staff.user_id} value={staff.user_id}>
-            {staff.first_name} {staff.last_name}
-          </option>
-        ))}
-            </select>
-          </div>
-          <div className="form-group">
-            <label className="label" htmlFor="task_title">
-             Title :
-            </label>
-            <input type="text" className="form-control" placeholder="Enter title for task"
-              id="task_title"
-              value={taskTitle}
-              onChange={handleTitleChange}
-            >
-            </input>
-          </div>
-          <div className="form-group">
-            <label className="label" htmlFor="dueDate">
-              Due Date:
-            </label>
-            <input
-              type="date"
-              id="dueDate"
-              value={dueDate}
-              onChange={handleDueDateChange}
-              className="input-box"
-            />
-          </div>
-          <div className="form-group">
-            <label className="label" htmlFor="taskDescription">
-              Task Description:
-            </label>
-            <textarea
-              id="taskDescription"
-              value={taskDescription}
-              onChange={handleDescriptionChange}
-              className="textarea-box"
-            ></textarea>
-
     <div className="main-container">
       <div className="row card-container">
         <div className="staff-dropdown-container">
